ce315fb7929f66bd1bcd99586a70a1391ac5118d
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-15 16:00:01 +0000

    added links to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,23 @@
-
+import { useRouter } from "next/router"
 
 function Nav() {
+
+  const router = useRouter()
+
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav content">
+        <div className="nav-left">
+          <a href="https://www.ryanbriggs.dev/" target='_blank noreferrer'>Portfolio</a>
+          <li onClick={() => router.push('/')}>Home</li>
+        </div>
+        <div className="nav-right">
+          <li>Small</li>
+          <li>Medium</li>
+          <li>Large</li>
+        </div>
+      </div>
+    </nav>
   )
 }
 
