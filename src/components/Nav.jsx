865e71bd434f0bcc39bd748ad0b807f9b2c5fe0c
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-16 13:19:54 +0000

    small edit to burger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,9 +20,9 @@ function Nav() {
             <div className="burger-line"></div>
           </div>
           <ul className="nav-links" id={navToggle ? 'show' : ''}>
-            <li>Small {navToggle ? 'Components' : ''}</li>
-            <li>Medium {navToggle ? 'Components' : ''}</li>
-            <li>Large {navToggle ? 'Components' : ''}</li>
+            <li>Small{navToggle ? ' Components' : ''}</li>
+            <li>Medium{navToggle ? ' Components' : ''}</li>
+            <li>Large{navToggle ? ' Components' : ''}</li>
           </ul>
         </div>
       </div>
