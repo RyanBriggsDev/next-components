1b2aede57a74f01f2c3be4689cdad2358e995b66
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-16 21:42:11 +0000

    steps finished

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,31 +3,32 @@ import { useState } from "react"
 
 function Nav() {
 
-  const router = useRouter()
-  const [navToggle, setNavToggle] = useState(false)
+  // const router = useRouter()
+  // const [navToggle, setNavToggle] = useState(false)
 
-  return (
-    <nav>
-      <div className="nav content">
-        <div className="nav-left">
-          <a href="https://www.ryanbriggs.dev/" target='_blank noreferrer'>Portfolio</a>
-          <li onClick={() => router.push('/')}>Home</li>
-        </div>
-        <div className="nav-right">
-        <div className={`hamburger ${navToggle === false ? '' : 'open'}`} onClick={(() => setNavToggle(!navToggle))}>
-            <div className="burger-line"></div>
-            <div className="burger-line"></div>
-            <div className="burger-line"></div>
-          </div>
-          <ul className="nav-links" id={navToggle ? 'show' : ''}>
-            <li>Small{navToggle ? ' Components' : ''}</li>
-            <li>Medium{navToggle ? ' Components' : ''}</li>
-            <li>Large{navToggle ? ' Components' : ''}</li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+  // return (
+  //   <nav>
+  //     <div className="nav content">
+  //       <div className="nav-left">
+  //         <a href="https://www.ryanbriggs.dev/" target='_blank noreferrer'>Portfolio</a>
+  //         <li onClick={() => router.push('/')}>Home</li>
+  //       </div>
+  //       <div className="nav-right">
+  //       <div className={`hamburger ${navToggle === false ? '' : 'open'}`} onClick={(() => setNavToggle(!navToggle))}>
+  //           <div className="burger-line"></div>
+  //           <div className="burger-line"></div>
+  //           <div className="burger-line"></div>
+  //         </div>
+  //         <ul className="nav-links" id={navToggle ? 'show' : ''}>
+  //           <li>Small{navToggle ? ' Components' : ''}</li>
+  //           <li>Medium{navToggle ? ' Components' : ''}</li>
+  //           <li>Large{navToggle ? ' Components' : ''}</li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </nav>
+  // )
+  return <></>
 }
 
 export default Nav
--- src/pages/components/navbar/index.jsx
+++ src/pages/components/navbar/index.jsx
@@ -8,7 +8,9 @@ import two from '../../../assets/img/pages/navPage/Two.png'
 import three from '../../../assets/img/pages/navPage/three.png'
 import four from '../../../assets/img/pages/navPage/four.png'
 import five from '../../../assets/img/pages/navPage/five.png'
-
+import six from '../../../assets/img/pages/navPage/six.png'
+import seven from '../../../assets/img/pages/navPage/seven.png'
+import eight from '../../../assets/img/pages/navPage/eight.png'
 
 function NavbarPage() {
   return (
@@ -20,7 +22,7 @@ function NavbarPage() {
         <TestNav />
         <section className="my-4">
           <Steps 
-            stepNo='One'
+            stepNo='1'
             title='Make a New Component'
             img={one}
             desc={`First we need to make a new component. Typical file structure is src/components/nav.jsx. But you can do whatever suits your needs. We'll be using a React functional component.`}
@@ -52,9 +54,22 @@ function NavbarPage() {
             <Steps 
               stepNo='6'
               title={`Toggle State & Conditional Classes`}
-              img={five}
+              img={six}
               desc={`Now we've got all the JSX elements we need we need to add some state to track if the hamburger menu should be open. Let's call this navToggle. The default value should be false (closed). Then we need an onClick prop that'll update the state to the opposite of the state. Our div.hamburger should also have a conditional class 'open' that applied if our navToggle state is true. We should also have a conditional id 'show' for the nav links ul.`}
             />
+            <Steps 
+              stepNo='7'
+              title='Media Queries'
+              img={seven}
+              desc={`Choose a max-width that's right for your navbar. I've chose 40rem for this tutorial but it'll depend of your navs content. Change the ul to display none within the media query. So that below your max-width they will not show by default. We can then target the #show id which will become active when we click our hamburger. We can set display flex, with a position of absolute, left 0 and top to be positioned underneath your nav. The width will be 100%, height auto and any other styling as you wish. We will need to set the hamburger div's display as default to none and within the media query as flex. You should now have a functional responsive navbar.`}
+            />
+            <Steps 
+              stepNo='8'
+              title='Hamburger Animation (style points)'
+              img={eight}
+              desc={`This is easier than it looks, and it gives off a really cool effect. You don't have to do it the same as mine, but here is how I do it. Target each burger line individually with the nth-child pseudo element. We can then add a transform property that'll rotate/move the elements as we wish when the burger is clicked. Just remember to add a transition too! (I use ease-in-out)`}
+            />
+            <p>And that's it. You'll now have a fully responsive and functional navbar component. If this helped you at all, or you have any improvements (I know they'll be plenty), shout at me on Twitter <a className="inline-link" href="https://twitter.com/RyanBriggsDev" target='_blank noreferrer'>@ryanbriggsdev</a>.</p>
         </section>
     </>
   )
