3e4c1bc576900435bd4d13ccd39c95493d90dcc7
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-11-20 20:32:31 +0000

    removed duplicate entries

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,13 @@ function Footer() {
   return (
     <footer className="content">
       <div className="footer grid-6-col g-1">
-        <FooterSection />
+        <FooterSection 
+          title='Site Map'
+        />
         <FooterSection
           title='Components' 
           footerArr={['All Components', 'Navbar', 'Card', 'Form', 'Page Layout']}
         />
-        <FooterSection />
-        <FooterSection />
-        <FooterSection />
-        <FooterSection />
       </div>
     </footer>
   )
